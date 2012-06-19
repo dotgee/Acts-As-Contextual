85ad7227c26d63eae0f6107bf7a84e43943148ba
Tanguy Antoine <antoine.tanguy35@gmail.com>
2012-06-19 14:33:38 +0200

    ajout de la configuration

--- lib/acts_as_contextual.rb
+++ lib/acts_as_contextual.rb
@@ -1,6 +1,7 @@
 require "acts_as_contextual/version"
 require 'active_record'
 
+require "acts_as_contextual/config"
+require "acts_as_contextual/acts_as_contextual"
 module ActsAsContextual
-  require "acts_as_contextual/acts_as_contextual"
 end
