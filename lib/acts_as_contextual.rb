487aadba5890f0329feae60a0fa7a35388895a38
Tanguy Antoine <antoine.tanguy35@gmail.com>
2012-06-20 10:01:57 +0200

    oublie d'un fichier

--- lib/acts_as_contextual.rb
+++ lib/acts_as_contextual.rb
@@ -2,6 +2,8 @@ require "acts_as_contextual/version"
 require 'active_record'
 
 require "acts_as_contextual/config"
+require "acts_as_contextual/contextualizable"
 require "acts_as_contextual/acts_as_contextual"
+require "acts_as_contextual/acts_as_contextualizer"
 module ActsAsContextual
 end
