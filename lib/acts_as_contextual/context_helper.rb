ad0e5f6ca6ecd3fb9fc430b6e288fd95ae0af697
Rogersss <jroger@dotgee.fr>
2012-07-03 14:23:33 +0200

    modification tri

--- lib/acts_as_contextual/context_helper.rb
+++ lib/acts_as_contextual/context_helper.rb
@@ -2,7 +2,7 @@ module ActsAsContextual
   module ContextHelper
     def each_in_context(contextualized_klass, opts = {}, &block)
       order_by = opts.delete(:order_by) || :id
-      eachs = contextualized_klass.each_with_context(opts).sort(order_by)
+      eachs = contextualized_klass.each_with_context(opts).order(order_by)
       eachs = eachs.to_a.select{|c,itms| !c.nil? }
       sort = opts.delete(:context_sort) || :id
       sort_order = opts.delete(:reverse) || false
